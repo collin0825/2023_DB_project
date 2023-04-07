7708bcf50a8875b8eacef3a2acc353044e43508d
collin <collin0825@gmail.com>
2023-04-07 20:01:55 +0800

    project init

---
+++ link.py
@@ -0,0 +1,4 @@
+import cx_Oracle
+# cx_Oracle.init_oracle_client(lib_dir="./instantclient_19_8") # init Oracle instant client 位置
+connection = cx_Oracle.connect('GROUP1', '2IGVB44zhK', cx_Oracle.makedsn('140.117.69.70', 1521, service_name='ORCLPDB1'))
+cursor = connection.cursor()
